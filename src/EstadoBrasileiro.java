40d7aefe6734fac16cd497521edef34a4b91a13c
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-08-08 18:19:47 -0300

    java-basico

---
+++ src/EstadoBrasileiro.java
@@ -0,0 +1,30 @@
+
+public enum EstadoBrasileiro {
+	SAO_PAULO ("SP", "São Paulo",1),
+	RIO_JANEIRO("RJ", "Rio de Janeiro",2),
+	PIAUI ("PI", "Piaui",3),
+	MARANHAO ("MA", "Maranhão",4),
+	CEARA("CE", "Ceara",5);
+	
+	private String nome;
+	private String sigla;
+	private int ibge;
+	
+	private EstadoBrasileiro(String sigla, String nome, int ibge) {
+		this.sigla = sigla;
+		this.nome = nome;
+		this.ibge = ibge;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public int getIbge() {
+		return ibge;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getNomeMaiusculo() {
+		return nome.toUpperCase();
+	}
+}
